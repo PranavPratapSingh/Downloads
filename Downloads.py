426199f36481f2a5582f73f4806a64dfa82e8382
Pranav Pratap Singh <1297pav@gmail.com>
2018-10-24 22:50:09 +0530

    modified:   Downloads.py

--- Downloads.py
+++ Downloads.py
@@ -1,7 +1,7 @@
 import glob
 import shutil
 import os
-
+#taking inputs
 extension = raw_input("Enter the Extensions you want to process, separated by space: ")
 ext = extension.split(" ")
 direc = raw_input("Enter Absolute Path of Target Directory, or leave Blank for Downloads: ")
@@ -11,10 +11,11 @@ if direc == "":
 else:
     direc = direc + "/"
 os.chdir(direc)
+#making folders and moving files
 for E in ext:
     if not os.path.exists(E):
         os.makedirs(E)
     for f in glob.glob("*."+E):
         shutil.move(f, E+"/"+f)
-
+#end of program
 print "Operation Successful"
